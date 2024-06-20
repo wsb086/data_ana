7dd4330bfa35ebea540a4b1e157148696d54779f
wsb086 <156738767+wsb086@users.noreply.github.com>
2024-06-20 18:18:31 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -29,30 +29,29 @@ else:
 if df is not None:
     st.write(df)
     # 选择ID变量
-id_var = st.selectbox("选择ID变量", df.columns)
-
-# 选择因变量
-target_var = st.selectbox("选择因变量", [col for col in df.columns if col != id_var])
-
-# 选择自变量
-default_features = [col for col in df.columns if col not in [id_var, target_var]]
-selected_features = st.multiselect("选择自变量", default_features, default=default_features)
-df_s=df[selected_features+[id_var,target_var]]
-dataset = TabularDataset(df_s)
-predictor = TabularPredictor(label=target_var, problem_type='regression').fit(dataset,hyperparameters={'GBM':{},'XGB':{}})
-predictions = predictor.predict(dataset)
-st.write('模型拟合成功!')
-def plot_feature_importance(model_name):
-    importance_df = predictor.feature_importance(data=dataset, model=model_name)
-    top10_features = importance_df.head(10)
-    myfig=sns.barplot(x=top10_features['importance'], y=top10_features.index)
-    myfig.set_title(f'{model_name} Top 10 Features')
-    myfig.set_xlabel('Importance')
-    myfig.set_ylabel('Features')
-    plt.figure(figsize=(10, 6))
-    st.pyplot(plt.gcf())
-plot_feature_importance('WeightedEnsemble_L2')
-
+    id_var = st.selectbox("选择ID变量", df.columns)
+    
+    # 选择因变量
+    target_var = st.selectbox("选择因变量", [col for col in df.columns if col != id_var])
+    
+    # 选择自变量
+    default_features = [col for col in df.columns if col not in [id_var, target_var]]
+    selected_features = st.multiselect("选择自变量", default_features, default=default_features)
+    df_s=df[selected_features+[id_var,target_var]]
+    dataset = TabularDataset(df_s)
+    predictor = TabularPredictor(label=target_var, problem_type='regression').fit(dataset,hyperparameters={'GBM':{},'XGB':{}})
+    predictions = predictor.predict(dataset)
+    st.write('模型拟合成功!')
+    def plot_feature_importance(model_name):
+        importance_df = predictor.feature_importance(data=dataset, model=model_name)
+        top10_features = importance_df.head(10)
+        myfig=sns.barplot(x=top10_features['importance'], y=top10_features.index)
+        myfig.set_title(f'{model_name} Top 10 Features')
+        myfig.set_xlabel('Importance')
+        myfig.set_ylabel('Features')
+        plt.figure(figsize=(10, 6))
+        st.pyplot(plt.gcf())
+    plot_feature_importance('WeightedEnsemble_L2')
 
 else:
     st.write("请上传文件或者使用默认数据")
