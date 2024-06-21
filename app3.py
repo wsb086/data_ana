c5924377f9dd947d7f105bf27596c64ca25dbbac
wsb086 <156738767+wsb086@users.noreply.github.com>
2024-06-21 11:30:46 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -22,70 +22,95 @@ if 'dataset' not in st.session_state:
     st.session_state.dataset = False
 if 'model_condition' not in st.session_state:
     st.session_state.model_condition = False
-use_default_data = st.button("使用默认数据")
 
-uploaded_file = st.file_uploader("上传 CSV 或 XLSX 文件", type=["csv", "xlsx"])
+# 创建一个导航菜单
+page = st.sidebar.selectbox("导航", ["Home", "模型拟合", "模型解释"])
 
-if use_default_data:
-    st.session_state.df = pd.read_csv("data_alive.csv")
-    st.write('数据读取成功！')
-    st.write(st.session_state.df.head(5))
-    st.session_state.data_condition = True
-elif uploaded_file is not None:
-    if uploaded_file.name.endswith('.csv'):
-        st.session_state.df = pd.read_csv(uploaded_file)
+if page == "Home":
+    st.title("欢迎使用数据分析工具")
+    st.write("""
+    ### 介绍
+    这是一个用于数据分析和机器学习模型拟合与解释的工具。你可以通过侧边栏导航到不同的功能页面。
+
+    ### 使用说明
+    1. 在模型拟合页面，你可以上传自己的数据集，选择自变量和因变量，并进行模型拟合。
+    2. 在模型解释页面，你可以查看模型的解释结果。
+
+    请在侧边栏选择相应的功能页面开始使用。
+    """)
+
+elif page == "模型拟合":
+    st.title("模型拟合")
+
+    use_default_data = st.button("使用默认数据")
+
+    uploaded_file = st.file_uploader("上传 CSV 或 XLSX 文件", type=["csv", "xlsx"])
+
+    if use_default_data:
+        st.session_state.df = pd.read_csv("data_alive.csv")
         st.write('数据读取成功！')
+        st.write(st.session_state.df.head(5))
         st.session_state.data_condition = True
+    elif uploaded_file is not None:
+        if uploaded_file.name.endswith('.csv'):
+            st.session_state.df = pd.read_csv(uploaded_file)
+            st.write('数据读取成功！')
+            st.session_state.data_condition = True
+        else:
+            st.session_state.df = pd.read_excel(uploaded_file)
+            st.write('数据读取成功！')
+            st.session_state.data_condition = True
+
+    if st.session_state.data_condition:
+        df = st.session_state.df
+        id_var = st.selectbox("选择ID变量", df.columns)
+        target_var = st.selectbox("选择因变量", [col for col in df.columns if col != id_var])
+        default_features = [col for col in df.columns if col not in [id_var, target_var]]
+        selected_features = st.multiselect("选择自变量", default_features, default=default_features)
+
+        fit_begin = st.button("开始拟合模型！")
+
+        if fit_begin:
+            df_s = df[selected_features + [target_var]]
+            st.session_state.dataset = TabularDataset(df_s)
+            predictor = TabularPredictor(label=target_var, problem_type='regression').fit(st.session_state.dataset, hyperparameters={'GBM': {}, 'XGB': {}})
+            st.session_state.predictor = predictor
+            st.session_state.model_fitted = True
+            st.write('模型拟合成功!')
+
+            def plot_feature_importance(model_name):
+                importance_df = predictor.feature_importance(data=st.session_state.dataset, model=model_name)
+                top10_features = importance_df.head(10)
+                plt.figure(figsize=(10, 6))
+                sns.barplot(x=top10_features['importance'], y=top10_features.index)
+                plt.title(f'{model_name} Top 10 Features')
+                plt.xlabel('Importance')
+                plt.ylabel('Features')
+                st.pyplot(plt.gcf())
+            
+            # 在一个容器中显示特征重要性图
+            with st.container():
+                plot_feature_importance('WeightedEnsemble_L2')
+            
+            st.session_state.model_condition = True
     else:
-        st.session_state.df = pd.read_excel(uploaded_file)
-        st.write('数据读取成功！')
-        st.session_state.data_condition = True
+        st.write("请上传文件或者使用默认数据")
 
-if st.session_state.data_condition:
-    df = st.session_state.df
-    id_var = st.selectbox("选择ID变量", df.columns)
-    target_var = st.selectbox("选择因变量", [col for col in df.columns if col != id_var])
-    default_features = [col for col in df.columns if col not in [id_var, target_var]]
-    selected_features = st.multiselect("选择自变量", default_features, default=default_features)
+elif page == "模型解释":
+    st.title("模型解释")
 
-    fit_begin = st.button("开始拟合模型！")
-
-    if fit_begin:
-        df_s = df[selected_features + [target_var]]
-        st.session_state.dataset = TabularDataset(df_s)
-        predictor = TabularPredictor(label=target_var, problem_type='regression').fit(st.session_state.dataset, hyperparameters={'GBM': {}, 'XGB': {}})
-        st.session_state.predictor = predictor
-        st.session_state.model_fitted = True
-        st.write('模型拟合成功!')
-
-        def plot_feature_importance(model_name):
-            importance_df = predictor.feature_importance(data=st.session_state.dataset, model=model_name)
-            top10_features = importance_df.head(10)
-            plt.figure(figsize=(10, 6))
-            sns.barplot(x=top10_features['importance'], y=top10_features.index)
-            plt.title(f'{model_name} Top 10 Features')
-            plt.xlabel('Importance')
-            plt.ylabel('Features')
-            st.pyplot(plt.gcf())
-        
-        # 在一个容器中显示特征重要性图
-        with st.container():
-            plot_feature_importance('WeightedEnsemble_L2')
-        
-        st.session_state.model_condition = True
-else:
-    st.write("请上传文件或者使用默认数据")
-
-if st.session_state.model_condition:
-    shap_explain = st.button("开始解释")
-    if shap_explain:
-        model_to_explain = st.session_state.predictor._trainer.load_model('WeightedEnsemble_L2')
-        background_data = st.session_state.dataset.sample(n=5000, random_state=1)
-        explainer = shap.Explainer(model_to_explain.predict, background_data)
-        sample_data = st.session_state.dataset.sample(n=10)
-        shap_values = explainer(sample_data)
-        
-        # 在一个新容器中显示 SHAP 图
-        with st.container():
-            shap.summary_plot(shap_values, sample_data)
-            st.pyplot(bbox_inches='tight')
+    if st.session_state.model_condition:
+        shap_explain = st.button("开始解释")
+        if shap_explain:
+            model_to_explain = st.session_state.predictor._trainer.load_model('WeightedEnsemble_L2')
+            background_data = st.session_state.dataset.sample(n=5000, random_state=1)
+            explainer = shap.Explainer(model_to_explain.predict, background_data)
+            sample_data = st.session_state.dataset.sample(n=10)
+            shap_values = explainer(sample_data)
+            
+            # 在一个新容器中显示 SHAP 图
+            with st.container():
+                shap.summary_plot(shap_values, sample_data)
+                st.pyplot(bbox_inches='tight')
+    else:
+        st.write("请先完成模型拟合")
