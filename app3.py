635e6ba29d81198d27e9eae896070aa48d018a6d
wsb086 <156738767+wsb086@users.noreply.github.com>
2024-06-21 11:51:21 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -114,10 +114,10 @@ elif page == "模型解释":
             
             # 在一个新容器中显示 SHAP 图
             with st.container():
-                st.subtitle('summary_plot')
+                st.subheader('summary_plot')
                 shap.summary_plot(shap_values, sample_data)
                 st.pyplot(bbox_inches='tight')
-                st.subtitle('bar_plot')
+                st.subheader('bar_plot')
                 shap.plots.bar(shap_values)
                 st.pyplot(bbox_inches='tight')
     else:
